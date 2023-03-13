74a95122313d5b8cbf0603c12293dcc9e33c996a
Ahmad Kabani <ahmadkabani@yahoo.com>
2023-03-12 20:16:51 -0400

    add shift-h and shift-l to move between buffers

--- mappings.lua
+++ mappings.lua
@@ -22,6 +22,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+    ["<S-h>"] = { ":bp<cr>", desc = "Previous buffer" },
+    ["<S-l>"] = { ":bn<cr>", desc = "Next buffer" },
   },
   t = {
     -- setting a mapping to false will disable it
